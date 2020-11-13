07c53359d435a8f8070310604ca9f6f71ef6bbd9
Perizat <karina.perizat@epam.com>
2020-11-13 13:09:57 +0600

    m2t1

--- m2/DebuggingFundamentals/Task1/Product.cs
+++ m2/DebuggingFundamentals/Task1/Product.cs
@@ -11,5 +11,26 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            //if obj is not null and type of obj is Product
+            if (!(obj is null) && obj is Product)
+            {
+                Product productIn = (Product)obj;
+                Product productThis = this;
+                return productIn.Name == productThis.Name && productIn.Price == productThis.Price;
+            }
+
+            return false;
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj is null) return false;
+        //    if (!(obj is Product)) return false;
+        //    var product = (Product)obj;
+        //    return product.Name == this.Name && product.Price == this.Price;
+        //}
     }
 }
